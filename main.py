17d5abc9ea44bb84f1aa0fcd0d27d320f3bcbebd
jkoniecznyy <jerkon14@gmail.com>
2021-10-23 15:33:35 +0200

    Add task03

--- main.py
+++ main.py
@@ -1,37 +1,67 @@
-
-def printNames(names):
-    for name in names:
-        print(name)
+def sayHi(name: str, surname: str) -> str:
+    return f'Czesc {name} {surname}!'
 
 
-def multiplyBy2(numbers):
-    newNumbers = []
-    for number in numbers:
-        newNumbers.append(number * 2)
-    return newNumbers
+def multiply(nr1: int, nr2: int) -> int:
+    return nr1 * nr2
 
 
-def multiplyBy2Comprehension(numbers):
-    return [number * 2 for number in numbers]
+def evenCheck(number: int) -> bool:
+    if number % 2 == 0:
+        return True
+    return False
 
 
-def printEven(numbers):
-    for number in range(len(numbers)):
-        if numbers[number] % 2 == 0:
-            print(numbers[number])
+def task4(nr1: int, nr2: int, nr3: int) -> bool:
+    if nr1 + nr2 >= nr3:
+        return True
+    return False
 
 
-def printEvenIndexes(numbers):
-    for index in range(0, len(numbers), 2):
-            print(numbers[index])
+def findInList(numbers: list, number: int) -> None:
+    flag = False
+    for n in numbers:
+        if n == number:
+            flag = True
+            break
+    if flag:
+        print('The number was in the list')
+    else:
+        print('The number was not in the list')
 
-cpNames = ['Johnny', 'V', 'Jackie [*]', 'Judy', 'Panam']
-someNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-printNames(cpNames)
-print(multiplyBy2(someNumbers))
-print(multiplyBy2Comprehension(someNumbers))
-print('printEven')
-printEven(someNumbers)
-print('printEvenIndexes')
-printEvenIndexes(someNumbers)
 
+def mergeLists(list1: list, list2: list) -> list:
+    result = set()
+
+    for l1 in list1:
+        result.add(l1)
+
+    for l2 in list2:
+        result.add(l2)
+
+    result = list(result)
+
+    for i, r in enumerate(result):
+        if type(r) is int or type(r) is float or type(r) is complex:
+            result[i] = r ** 3
+
+    print(result)
+    return result
+
+
+result = sayHi('Johnny', 'Silverhand')
+print(result)
+
+print(multiply(6, 8))
+
+result = evenCheck(6)
+if result:
+    print("Liczba parzysta")
+else:
+    print("Liczba nieparzysta")
+
+print(task4(1, 2, 8))
+
+findInList([1, 2, 3, 4, 5, 6, 7, 8, 9], 3)
+
+mergeLists([1, 2, 3, 4, 8, 'try', b'me'], [2, 3, 4, 5, 6, 7, 8, 6.7, 2j])
