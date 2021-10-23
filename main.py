7430226e25756cc8230c884243c511833705c495
jkoniecznyy <jerkon14@gmail.com>
2021-10-23 15:55:08 +0200

    Some improvments in task03

--- main.py
+++ main.py
@@ -7,27 +7,15 @@ def multiply(nr1: int, nr2: int) -> int:
 
 
 def evenCheck(number: int) -> bool:
-    if number % 2 == 0:
-        return True
-    return False
+    return number % 2 == 0
 
 
 def task4(nr1: int, nr2: int, nr3: int) -> bool:
-    if nr1 + nr2 >= nr3:
-        return True
-    return False
+    return nr1 + nr2 >= nr3
 
 
-def findInList(numbers: list, number: int) -> None:
-    flag = False
-    for n in numbers:
-        if n == number:
-            flag = True
-            break
-    if flag:
-        print('The number was in the list')
-    else:
-        print('The number was not in the list')
+def findInList(numbers: list, number: int) -> bool:
+    return number in numbers
 
 
 def mergeLists(list1: list, list2: list) -> list:
@@ -40,28 +28,35 @@ def mergeLists(list1: list, list2: list) -> list:
         result.add(l2)
 
     result = list(result)
-
     for i, r in enumerate(result):
         if type(r) is int or type(r) is float or type(r) is complex:
             result[i] = r ** 3
 
-    print(result)
     return result
 
 
+print("Task1")
 result = sayHi('Johnny', 'Silverhand')
 print(result)
 
+print("Task2")
 print(multiply(6, 8))
 
-result = evenCheck(6)
-if result:
-    print("Liczba parzysta")
-else:
-    print("Liczba nieparzysta")
+print("Task3")
+result = evenCheck(55)
+print("Liczba parzysta" if result else "Liczba nieparzysta")
+result = evenCheck(60)
+print("Liczba parzysta" if result else "Liczba nieparzysta")
 
+print("Task4")
 print(task4(1, 2, 8))
+print(task4(1, 2, 1))
 
-findInList([1, 2, 3, 4, 5, 6, 7, 8, 9], 3)
+print("Task5")
+fil = findInList([1, 2, 3, 4, 5, 6, 7, 8, 9], 3)
+print('The number was in the list' if fil else 'The number was not in the list')
+fil = findInList([1, 2, 3, 4, 5, 6, 7, 8, 9], 30)
+print('The number was in the list' if fil else 'The number was not in the list')
 
-mergeLists([1, 2, 3, 4, 8, 'try', b'me'], [2, 3, 4, 5, 6, 7, 8, 6.7, 2j])
+print("Task6")
+print(mergeLists([1, 2, 3, 4, 8, 'try', b'me'], [2, 3, 4, 5, 6, 7, 8, 9.9, 100, 6.7, 2j]))
