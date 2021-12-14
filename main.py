bd3375602ff8d7d68afb095923bc2916a0f5e268
jkoniecznyy <jerkon14@gmail.com>
2021-12-14 15:16:45 +0100

    task 9 init

--- main.py
+++ main.py
@@ -1,48 +1,19 @@
-from flask import Flask
-from flask_restful import Api, Resource
+import cv2
+import matplotlib.pyplot as plt
 
-from src.read_functions.read_functions import read_from_csv
+def main():
+    img = cv2.imread('Shrek.jpg')
+    print(type(img))
+    print(img)
+    print(img.shape)
+    cv2.imshow('image', img)
+    cv2.imwaitKey(0)
 
-app = Flask(__name__)
-api = Api(app)
-
-
-@app.route('/api/status', methods=['GET'])
-def getNetworkStatus():
-    """
-        :rtype: str
-    """
-    return 'The network is working properly'
-
-
-class Movies(Resource):
-
-    def get(self):
-        return [movie.__dict__ for movie in read_from_csv("Movie")]
+    img_convert = cv2.cvtColor(img, cv2.COLOR_BGRR2GRAY)
+    img.show(img_convert)
+    plt.show()
 
 
-class Links(Resource):
 
-    def get(self):
-        return [link.__dict__ for link in read_from_csv("Link")]
-
-
-class Ratings(Resource):
-
-    def get(self):
-        return [rating.__dict__ for rating in read_from_csv("Rating")]
-
-
-class Tags(Resource):
-
-    def get(self):
-        return [tag.__dict__ for tag in read_from_csv("Tag")]
-
-
-api.add_resource(Movies, '/api/movies')
-api.add_resource(Links, '/api/links')
-api.add_resource(Ratings, '/api/ratings')
-api.add_resource(Tags, '/api/tags')
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__ == 'main':
+    main()
