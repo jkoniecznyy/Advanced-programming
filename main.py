ba63cdb1070a41c906b2d2a06e55486efb072703
Jerzy Konieczny <62672370+jkoniecznyy@users.noreply.github.com>
2022-01-26 12:52:29 +0100

    people_count_v1

--- main.py
+++ main.py
@@ -4,10 +4,11 @@ from hog_person_detect import HogPersonDetect
 
 if __name__ == "__main__":
     hog = HogPersonDetect()
+
     for path in glob.glob("src/images/*.jpg"):
-        hog.person_detect(path, 'img')
+        hog.person_detect(path)
 
     for path in glob.glob("src/videos/*.mp4"):
-        hog.person_detect(path, 'vid')
+        hog.person_detect(path)
 
-    hog.person_detect('your_camera', 'vid')
+    hog.person_detect('your_camera.mp4')
--- src/hog_person_detect.py
+++ src/hog_person_detect.py
@@ -11,43 +11,25 @@ class HogPersonDetect:
         self._model.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
         self._regions = None
 
-    def _read_img(self, image_path: str):
-        # Read and resize the img from path
-        image = cv2.imread(image_path)
-        return imutils.resize(image, width=min(400, image.shape[1]))
-
-    def _show_result(self, image, regions, time):
-        # Mark people in the img
-        counter = 0
-        for (x, y, w, h) in regions:
-            cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 2)
-            counter += 1
-        print(f'Model detected {counter} people in {time} s.')
-        # Show given img
-        cv2.imshow('Image', image)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
-
-    def person_detect(self, path: str, type):
-        if type == 'img':
+    def person_detect(self, path: str):
+        # Use proper function depending on the filename
+        if path[-4:] == '.jpg':
             self._img_person_detect(path)
-        else:
+        elif path[-4:] == '.mp4':
             self._vid_person_detect(path)
 
     def _img_person_detect(self, image_path: str):
-        image = self._read_img(image_path)
-
-        timestamp = datetime.now()
-        # use the cv2.HOGDescriptor_getDefaultPeopleDetector to detect people on the img
-        (regions, _) = self._model.detectMultiScale(image, winStride=(4, 4), padding=(4, 4), scale=1.05)
-
-        time = float("{:.2f}".format((datetime.now() - timestamp).total_seconds()))
-
-        self._show_result(image, regions, time)
+        # Read img from provided path and call _use_model_on_img()
+        image = cv2.imread(image_path)
+        self._use_model_on_img(image)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
 
     def _vid_person_detect(self, vid_path: str):
+        # Read video from provided path and call _use_model_on_img()
+        # for every frame
         cv2.startWindowThread()
-        if vid_path == 'your_camera':
+        if vid_path == 'your_camera.mp4':
             cap = cv2.VideoCapture(0)
         else:
             cap = cv2.VideoCapture(vid_path)
@@ -56,26 +38,7 @@ class HogPersonDetect:
             # Read the video stream
             ret, image = cap.read()
             if ret:
-                image = imutils.resize(image,
-                                       width=min(400, image.shape[1]))
-
-                # Detecting all the regions
-                # in the Image that has a
-                # pedestrians inside it
-                (regions, _) = self._model.detectMultiScale(image,
-                                                            winStride=(4, 4),
-                                                            padding=(4, 4),
-                                                            scale=1.05)
-
-                # Drawing the regions in the
-                # Image
-                for (x, y, w, h) in regions:
-                    cv2.rectangle(image, (x, y),
-                                  (x + w, y + h),
-                                  (0, 0, 255), 2)
-
-                # Showing the output Image
-                cv2.imshow("Image", image)
+                self._use_model_on_img(image)
                 if cv2.waitKey(25) & 0xFF == ord('q'):
                     break
             else:
@@ -83,3 +46,27 @@ class HogPersonDetect:
 
         cap.release()
         cv2.destroyAllWindows()
+
+    def _use_model_on_img(self, image):
+        # Prepare img
+        image = imutils.resize(image, width=min(400, image.shape[1]))
+
+        timestamp = datetime.now()
+
+        # use the cv2.HOGDescriptor_getDefaultPeopleDetector to detect people on the img
+        (regions, _) = self._model.detectMultiScale(image, winStride=(4, 4), padding=(4, 4), scale=1.05)
+
+        time = float("{:.2f}".format((datetime.now() - timestamp).total_seconds()))
+
+        # Mark people in the img
+        counter = 0
+        for (x, y, w, h) in regions:
+            cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 2)
+            counter += 1
+
+        # Put results on the image
+        cv2.putText(image, f'People detected: {counter} in {time} s.',
+                    (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 0), 2)
+
+        # Show  img
+        cv2.imshow("Image", image)
